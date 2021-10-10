278e53799c43d222906471b364fb6cdcee9db58e
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-10-10 14:23:43 +0300

    add initial functionality to page

--- src/app/quotes-text/quotes-text.component.ts
+++ src/app/quotes-text/quotes-text.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Quote } from '../quotes';
 
+
 @Component({
   selector: 'app-quotes-text',
   templateUrl: './quotes-text.component.html',
   styleUrls: ['./quotes-text.component.css']
 })
 export class QuotesTextComponent implements OnInit {
+  
   quotes: Quote[] = [
-    {Word:'With blood comes knowledge',Author:'Arminius Octo',description:'Find an online version and watch merlin find his son'},
-    {Word:'In all thy ways acknowledge the Lord, and He shall direct thy paths.',Author:'King Solomon',description:'I have to buy cookies for the parrot'},
-    {Word:'Forsake her not and she will preserve thee, love her and she will keep thee.',Author:'Love tunes',description:'Diana has her birthday coming up soon'},
-    {Word:'Leave it to the Lord.',Author:'Nyutu Daniel',description:'Pupper likes expensive sancks'},
-    {Word:'Everything is connected in this universe',Author:'Socrates',description:'Damn Math'},
-    {Word:'Do not gain the world and loose your soul, wisdom is better than gold.',Author:'Bob Marley',description:'Cause I am an evil overlord'},
+    new Quote('With blood comes knowledge','Arminius Octo'),
+    new Quote('In all thy ways acknowledge the Lord, and He shall direct thy paths.','King Solomon'),
+    new Quote('Forsake her not and she will preserve thee, love her and she will keep thee.','Love tunes'),
+    new Quote('Leave it to the Lord.','Nyutu Daniel'),
+    new Quote('Everything is connected in this universe','Socrates'),
+    new Quote('Do not gain the world and loose your soul, wisdom is better than gold.','Bob Marley')
   ];
+  toggleDetails(index: any){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+   }
   constructor() { }
 
   ngOnInit(): void {
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,5 +1,23 @@
 export class Quote {
-     Word!: string;
+     /** Word!: string;
      Author: string=" ";
-     description: string=" "; 
+     description: string=" ";  */
+     showDescription: boolean;
+    constructor(public Word: string,public Author : string,public description: any){
+      this.showDescription=false;
+    }
  }
+ let description = {
+    upvotes: 0,
+    downvotes: 0,
+    time: Date(),
+    upvote: upvote(),
+    downvote: downvote(),
+
+}
+function upvote(){
+    alert("hello")
+}
+function downvote(){
+    alert("hello")
+}

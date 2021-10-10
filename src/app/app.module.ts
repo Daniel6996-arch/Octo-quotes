3be3790e558e9c4bf5e675c244f5f59b8d13478e
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-10-10 13:41:06 +0300

    add edits to page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuotesTextComponent } from './quotes-text/quotes-text.component';
 import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     QuotesTextComponent,
-    QuotesFormComponent
+    QuotesFormComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
